81d8febf15ef011eea9f66c750ee852193af1cdb
Luth-Badila <FrancescoLutfi10@gmail.com>
2023-06-22 17:58:25 +0700

    deleted nav and card components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup>
 import Navbar from "./components/Navbar.vue";
-import Nav from "./components/Nav.vue";
 </script>
 
 <template>
-  <!-- <Nav /> -->
   <navbar />
 </template>
--- src/components/Card.vue
+++
@@ -1,7 +0,0 @@
-<template>I'm a Card</template>
-
-<script>
-export default {};
-</script>
-
-<style></style>
